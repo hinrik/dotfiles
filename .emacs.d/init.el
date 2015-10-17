441ced9bf239f6230c5397cccfbea370ff22ee26
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2015-10-18 01:14:39 +0200

    Use helm-M-x

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -622,6 +622,7 @@ i.e. change right window to bottom, or change bottom window to right."
         helm-recentf-fuzzy-match t)
   :config
   (use-package helm-config
+    :bind ("M-x" . helm-M-x)
     :config
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)) ; make TAB work in terminal
